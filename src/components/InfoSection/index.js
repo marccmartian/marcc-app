662658bb12f0f62da92aa8269cc96dd072a7d2b4
Marvin Carrasco <marvincarrascoc@yahoo.com>
2020-12-10 00:18:16 -0500

    Adding the content text in about-me and the-musician sections

--- src/components/InfoSection/index.js
+++ src/components/InfoSection/index.js
@@ -34,20 +34,21 @@ const InfoSection = () => {
         <Column>
           <InfoTitle>Acerca de mí</InfoTitle>
           <InfoText>
-            Lorem ipsum odor amet, consectetuer adipiscing elit. Ac purus in
-            massa egestas mollis varius; dignissim elementum. Mollis tincidunt
-            mattis hendrerit dolor eros enim, nisi ligula ornare. Hendrerit
-            parturient habitant pharetra rutrum gravida porttitor eros feugiat.
-            Mollis elit sodales taciti duis praesent id. Consequat urna vitae
-            morbi nunc congue.
+            Ingeniero de Sistemas, amante de la tecnología, de la buena música y
+            del deporte. Con toda una vida profesional aun por recorrer,
+            tendencia a la resolución de problemas, trabajo en equipo,
+            pensamiento estratégico y enfocado. Preparado como Fullstack Web
+            Developer con conocimientos en HTML5, CSS3, JavaScript, React,
+            Redux, Git, Github, SQL, Ruby, Ruby on Rails y más... "El límite lo
+            pones tu." ✌😉
           </InfoText>
           <InfoText>
-            Non etiam tempor id arcu magna ante eget. Nec per posuere cubilia
-            cras porttitor condimentum orci suscipit. Leo maecenas in tristique,
-            himenaeos elementum placerat. Taciti rutrum nostra, eget cursus
-            velit ultricies. Quam molestie tellus himenaeos cubilia congue
-            vivamus ultricies. Interdum praesent ut penatibus fames eros ad
-            consectetur sed.
+            En la programación encontré una de mis pasiones, un espacio donde se
+            puede ser creativo, un lugar donde las ideas fluyen y puedes
+            desafiarte a ti mismo a mejorarlas. Como "Coder" el aprendizaje
+            nunca se detiene, quiero asumir nuevos retos que me lleven a ser
+            parte de la creación de soluciones tecnológicas que sean de ayuda
+            para la sociedad.
           </InfoText>
         </Column>
       </InfoWrapper>
